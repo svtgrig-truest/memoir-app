d322c5ca21feffdaccfda9297789d091a74791d9
svtgrig <110480590+svtgrig-truest@users.noreply.github.com>
2026-03-19 16:44:58 +0000

    feat: /family/dashboard/book page

---
+++ app/family/dashboard/book/page.tsx
@@ -0,0 +1,5 @@
+import { BookEditor } from '@/components/BookEditor';
+
+export default function FamilyBookPage() {
+  return <BookEditor backHref="/family/dashboard" backLabel="Семейный архив" />;
+}
